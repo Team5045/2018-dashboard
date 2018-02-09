3f0ab1af115e361ef0ceef96ccb0283eb7b456d9
Will Eggleston <work.willeggleston@gmail.com>
2018-02-08 18:01:10 -0600

    charts work... kinda

--- src/data.js
+++ src/data.js
@@ -1,7 +1,59 @@
 var table = document.getElementById("mytable");
-var dataset = {};
-var pids = {};
+var errorData = [{x:0,y:0}];
+var setpointData = [{x:0,y:0}];
+var valueData = [{x:0,y:0}];
 var j = 0;
+var time1 = Date.now();
+var time2 = Date.now();
+var time3 = Date.now();
+
+function debounce(fn, delay) {
+    var timer = null;
+    return function() {
+        var context = this, args = arguments
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            fn.apply(context, args);
+        },delay);
+    };
+}
+var lineChart = new Chart('myChart', {
+    type: 'line',
+    data: {
+        datasets:[
+            {
+                label: "Error",
+                borderColor	: 'red', 
+                backgroundColor	: 'red', 
+                data: errorData,
+                fill: false
+            },
+            {
+                label: "Setpoint",
+                borderColor	: 'grey',
+                backgroundColor	: 'grey',                 
+                data: setpointData,
+                fill: false
+            },
+            {
+                label: "Value",
+                borderColor	: 'black',
+                backgroundColor	: 'black',
+                data: valueData,
+                fill: false
+            }
+        ]
+    },
+    options: {  
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+});
+
 NetworkTables.addGlobalListener(function(key, value, isNew){
     if (isNew) {
         var row = table.insertRow();
@@ -16,59 +68,35 @@ NetworkTables.addGlobalListener(function(key, value, isNew){
         c2.addEventListener('blur', function(){
             NetworkTables.putValue(c2.parentElement.id,c2.textContent);
         })
-        if (['kP','kI','kD','kH'].includes(key.slice(-2))) {
-            pids[key.slice(0,-3)] = {
-                'value':1,
-                'setpoint':1,
-                'error':1,
-                'time': Date()
-            };
-            dataset[key.slice(0,-3)] = {
-                'value': [],
-                'setpoint': [],
-                'error': [],
-                'time': []
-            }
-        }
-        }
+    }
     else {
         var row = document.getElementById(key);
         row.cells[2].textContent = value;
-        var i = key.split('/').slice(0,-1).join('/');
-        if (Object.keys(pids).includes(i)) {
-            pids[i] = {
-                'value': NetworkTables.getValue(i+'value'),
-                'setpoint': NetworkTables.getValue(i+'setpoint'),
-                'error': NetworkTables.getValue(i+'error')
+        var i = key.split('/').slice(-1).join('/');
+        if (['value','setpoint','error'].includes(i)) {
+            if (i=='value'){
+                valueData.push({
+                    y: value,
+                    x: Date.now()-time1+valueData[valueData.length-1].y
+                });
+                time1 = Date.now()
             }
-            dataset[i]['value'].push({
-                x:j,
-                y:pids[i]['value']
-            });
-            dataset[i]['setpoint'].push({
-                x:j,
-                y:pids[i]['setpoint']
-            });
-            dataset[i]['error'].push({
-                x:j,
-                y:pids[i]['error']
-            });
-            j++;
-            var scatterChart = new Chart('myChart', {
-                type: 'line',
-                data: {
-                    datasets: dataset['/components/elevator']['value']
-                }, 
-                options: {  
-                    scales: {
-                        xAxes: [{
-                            type: 'linear',
-                            position: 'bottom'
-                        }]
-                    }
-                }
-            });
-            scatterChart.update(0);
+            else if (i=='setpoint'){
+                setpointData.push({
+                    y: value,
+                    x: Date.now()-time2+setpointData[setpointData.length-1].y
+                });
+                time2 = Date.now()     
+            }
+            else if (i=='error'){
+                errorData.push({
+                    y: value,
+                    x: Date.now()-time3+errorData[errorData.length-1].y
+                });
+                time3 = Date.now()
+            }
+            lineChart.update(0);
         }
+        
     }
 });
